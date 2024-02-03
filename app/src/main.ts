245c470bec1523777d38519f9658b15a81b528fc
Mateus <mfelini7@gmail.com>
2024-02-03 10:33:07 -0300

    feet: adicionado vuetify

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,14 +1,26 @@
+// Style
 import './assets/main.css'
+import 'vuetify/styles'
 
+// Modules
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
+// Config
 const app = createApp(App)
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
