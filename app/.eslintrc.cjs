da46f6f459a283bb60e0bd1341be92d109037acd
Mateus <mfelini7@gmail.com>
2024-02-03 10:20:36 -0300

    Eslint config

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -1,15 +1,39 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-strongly-recommended',
+        'prettier'
+    ],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script'
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint', 'vue'],
+    rules: {
+        semi: ['error', 'never'],
+        quotes: ['error', 'single'],
+        indent: ['error', 4],
+        'comma-spacing': ['error', { before: false, after: true }],
+        'linebreak-style': ['error', 'windows']
+    }
 }
