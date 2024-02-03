f8aa920c427d95ad872281545b5a2c6549bbed00
Mateus <mfelini7@gmail.com>
2024-02-03 11:12:40 -0300

    chore: Excluido arquivos não utilizados e adicionado sass

---
+++ app/src/components/Component.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="component"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
---
+++ app/src/views/login/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="login-view">
+        
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'LoginView'
+})
+</script>
